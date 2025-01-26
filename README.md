6015eceb824190bc508c19d837f1d9cc2fe493b4
Kostiantyn Rudenko <36504423+KosRud@users.noreply.github.com>
2025-01-26 17:47:37 +0100

    upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# isle-of-nerevir
+# neutralinojs-zero
+An empty Neutralinojs app, extend as you wish
+
+```
+neu create myapp --template neutralinojs/neutralinojs-zero
+```
---
+++ vue-src/src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import { init } from '@neutralinojs/lib';
+
+createApp(App).mount('#app');
+init();
